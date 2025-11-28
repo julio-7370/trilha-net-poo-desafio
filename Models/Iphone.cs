8285836d8768fe6603a3c2d79c09896d0bec49e4
Júlio <82112293+julio-7370@users.noreply.github.com>
2025-11-27 21:09:55 -0300

    Revisão final do código

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -3,14 +3,14 @@ namespace DesafioPOO.Models
     // TODO: Herdar da classe "Smartphone"
     public class Iphone : Smartphone
     {
-        public Iphone(string nome, string modelo, string imei, int memoria) : base(nome, modelo, imei, memoria)
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
-            
+
         }
         // TODO: Sobrescrever o método "InstalarAplicativo"
-        public override void InstalarAplicativo(string nome)
+        public override void InstalarAplicativo(string nomeApp)
         {
-            Console.WriteLine($"Instalando o a plicativo {nome} no IPhone");
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no IPhone");
         }
 
     }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -3,15 +3,15 @@ namespace DesafioPOO.Models
     // TODO: Herdar da classe "Smartphone"
     public class Nokia : Smartphone
     {
-        public Nokia(string nome, string modelo, string imei, int memoria) : base(nome, modelo, imei, memoria)
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
             
         }
 
         // TODO: Sobrescrever o método "InstalarAplicativo"
-        public override void InstalarAplicativo(string nome)
+        public override void InstalarAplicativo(string nomeApp)
         {
-            Console.WriteLine($"Instalando o a plicativo {nome} no Nokia");
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia");
         }
 
     }
